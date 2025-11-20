7ecc8fa8c74a6ad6fb4c0dba46fab85b6d8aa146
MrRichGamer12 <100773555+MrRichGamer12@users.noreply.github.com>
2025-11-20 14:48:36 +0000

    Views added

--- PWeb1_Projeto/src/router/index.js
+++ PWeb1_Projeto/src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePageView from '@/views/HomePageView.vue'
+import LoginPageView from '@/views/LoginPageView.vue'
+import RegisterPageView from '@/views/RegisterPageView.vue'
+import ProfilePageView from '@/views/ProfilePageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{ path: '/', component: HomePageView },
+  { path: '/login', component: LoginPageView },
+  { path: '/register', component: RegisterPageView },
+  { path: '/profile/:username', component: ProfilePageView, beforeEnter(to, from, next){
+  if (!UserAuhtenticated()) {
+    next('/login');
+  }else{
+    next();
+  }
+},}
+  ],
 })
-
 export default router
